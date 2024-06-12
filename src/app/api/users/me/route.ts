3aa70bd8899404679551bca3e1a92f4c867330a5
SuwasG <suwasghale693@gmail.com>
2024-06-12 15:01:01 +0545

    middleware and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,24 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect()
+
+export async function GET(request:NextRequest){
+    try{
+       const userId = await getDataFromToken(request);
+       const user = await User.findOne({_id:userId}).select("-password -isAdmin");
+       return NextResponse.json({
+        message:"User found.",
+        data:user
+       })
+
+    }
+    catch(error:any){
+        console.log(error)
+        return NextResponse.json({error:error.message}, {status:500})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,54 @@
+'use client';
+import axios from "axios";
+import Link from "next/link";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function ProfilePage(){
+    const router=useRouter();
+    const [data, setData] = useState()
+    const logout=async()=>{
+        try{
+          await axios.get('/api/users/logout')
+          toast.success("Logout Successful.")
+          router.push('/login')
+        }
+        catch(error:any){
+            console.log(error)
+            toast.error(error.message)
+        }
+    };
+
+    const getUserDetails = async()=>{
+       const res = await axios.get("/api/users/me")
+       console.log(res.data.data);
+       setData(res.data.data._id)
+    }
+
     return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <>
+        <Toaster position="top-center" reverseOrder={false}/>
+
+<div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr /><p>Profile Page</p>
+            <h2 className="p-2 rounded bg-blue-500 ">
+                {data === "nothing" ? "Nothing": <Link 
+                href={`/profile/${data}`}>{data}
+                </Link>}
+                </h2>
+            <hr />
+            <button 
+            onClick={logout}
+            className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-md mt-6">Logout</button>
+           
+            <button 
+            onClick={getUserDetails}
+            className="bg-purple-500 hover:bg-purple-600 text-white py-2 px-4 rounded-md mt-6">User Details</button>
         </div>
+
+        </>
+       
     )
 }
